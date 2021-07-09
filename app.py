2484ad0c14c8ac010af2aa366ca0f2f047b29127
MorningClub <35296910+MorningClub@users.noreply.github.com>
2021-07-09 14:13:14 +0200

    oppdatert

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def get_skattemelding(inntektsaar=None, personidentifikator=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5000)
--- jsontest.py
+++
@@ -1,7 +0,0 @@
-import json
-
-f = open('skattemeldinger.json',)
-data = json.load(f)
-f.close()
-
-print(data['skattemeldinger'])
